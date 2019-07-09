7b7347306e0ce53e22c256d3bb6fcd07a9977328
ishimaru-alt <51936419+ishimaru-alt@users.noreply.github.com>
2019-07-09 10:17:37 +0900

    Add files via upload

--- site/js/common.js
+++ site/js/common.js
@@ -1,7 +1,11 @@
-    $('.slider').slick({
-        centerMode: true,
-        centerPadding: '100px',
-        dots:true,
-        focusOnSelect:true,
-        autoplay:true,
-    });
+$(function(){
+ $('.sub_menu').hide();
+ $('.main_menu').click(function(){
+  $('ul.sub_menu').slideUp();
+  $('.main_menu').removeClass('open');
+  if($('+ul.sub_menu',this).css('display') == 'none'){
+   $('+ul.sub_menu',this).slideDown();
+   $(this).addClass('open');
+  }
+ });
+});
